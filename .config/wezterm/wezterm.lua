832c6007c71034502132c09afbf182aa2ac85b2c
stefanboca <stefan.r.boca@gmail.com>
2023-02-16 11:37:03 -0800

    update

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,39 +3,14 @@ return {
     font = wezterm.font {
         family = "Fira Code",
     },
-    enable_wayland = false,
-    front_end = "WebGpu",
+    font_size = 11,
     color_scheme = "Lunar",
     color_schemes = {
-        ["Lunar"] = {
-            foreground = "#c0caf5",
-            background = "#1a1b26",
-
-            cursor_bg = "#c0caf5",
-            cursor_fg = "#1a1b26",
-
-            cursor_border = "#c0caf5",
-
-            ansi = {
-                "#000000",
-                "#db4b4b",
-                "#41a6b5",
-                "#e0af68",
-                "#7dcfff",
-                "#9d7cd8",
-                "#0db9d7",
-                "#a9b1d6",
-            },
-            brights = {
-                "#000000",
-                "#f7768e",
-                "#73daca",
-                "#ff9e64",
-                "#89ddff",
-                "#bb9af7",
-                "#2AC3DE",
-                "#c0caf5",
-            },
-        }
-    }
+        ["Lunar"] = require("lunar"),
+    },
+    keys = {
+        { key = 'l', mods = 'ALT', action = wezterm.action.ShowLauncher },
+    },
+    enable_wayland = false,
+    front_end = "WebGpu",
 }
