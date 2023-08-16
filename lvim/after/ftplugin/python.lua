371aa66ace6067630857ca76ab63575e5cedd0ff
Stefan Boca <stefan.r.boca@gmail.com>
2023-08-16 12:01:56 -0700

    [lvim] more python things

--- lvim/after/ftplugin/python.lua
+++ lvim/after/ftplugin/python.lua
@@ -1,12 +1,17 @@
 -- require("lvim.lsp.manager").setup("ruff_lsp")
 require("lvim.lsp.manager").setup("pyright")
+-- require("lvim.lsp.manager").setup("jedi_language_server")
+-- require("lvim.lsp.manager").setup("pylyzer")
+-- require("lvim.lsp.manager").setup("pyre")
+-- require("lvim.lsp.manager").setup("pylsp")
 
 local null_ls = require("null-ls")
 
 require("lvim.lsp.null-ls.formatters").setup {
     null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.isort,
 }
 
 -- require "lvim.lsp.null-ls.linters".setup {
---     null_ls.builtins.diagnostics.ruff,
+--     null_ls.builtins.diagnostics.mypy,
 -- }
