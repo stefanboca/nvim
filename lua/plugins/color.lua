d3d5fae967f3386240983650538bedff2ed67eec
Stefan Boca <stefan.r.boca@gmail.com>
2023-10-21 14:41:38 -0700

    fix rainbow delimiters conifg

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -9,5 +9,18 @@ return {
     config = function(_, opts)
       require("rainbow-delimiters.setup").setup(opts)
     end,
+    opts = function(_, opts)
+      local rainbow_delimiters = require("rainbow-delimiters")
+      return {
+        strategy = {
+          [""] = rainbow_delimiters.strategy["global"],
+        },
+        query = {
+          [""] = "rainbow-delimiters",
+          latex = "rainbow-blocks",
+          lua = "rainbow-blocks",
+        },
+      }
+    end,
   },
 }
