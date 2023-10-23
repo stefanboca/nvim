4bffbb05ab5cd3946162228f6aa2551d447976f4
Stefan Boca <stefan.r.boca@gmail.com>
2023-10-23 12:23:47 -0700

    reorganize / update cmp mappings

---
+++ lua/plugins/completions.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      -- local luasnip = require("luasnip")
+      opts.completion.completeopt = "menu,menuone,noinsert,noselect"
+
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-j>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            cmp.complete()
+          end
+        end),
+        ["<C-k>"] = cmp.mapping(function()
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            cmp.complete()
+          end
+        end),
+        ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Insert, select = false }),
+        ["<S-CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+        -- <S-CR> is not passed to the shell by alacritty, so map a supertab for replace
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace })
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+}
