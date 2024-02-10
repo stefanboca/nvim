b385865448b4ca1ac739274228f4c849a5835b6e
Stefan Boca <stefan.r.boca@gmail.com>
2024-02-09 21:37:40 -0800

    wrap settings

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- When wrap is enabled, wrap on word boundaries
+vim.o.linebreak = true
