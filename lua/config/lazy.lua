1260845543b84dc046a956967e6f4ea7654ac3e3
Stefan Boca <stefan.r.boca@gmail.com>
2024-04-11 09:19:12 -0700

    yanky extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -21,6 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.util.project" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
 
     { import = "plugins" },
   },
