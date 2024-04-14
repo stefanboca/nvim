f6b10c60be19ffc37fbfccb9eaa6036789a11711
Stefan Boca <stefan.r.boca@gmail.com>
2024-04-13 21:38:14 -0700

    add rustaceanvim keybinds

---
+++ lua/plugins/rustaceanvim.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "mrcjkb/rustaceanvim",
+    -- dependencies = {
+    --   "folke/which-key.nvim",
+    -- },
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          local wk = require("which-key")
+
+          local function mapping(cmd)
+            return function()
+              vim.cmd.RustLsp(cmd)
+            end
+          end
+
+          wk.register({
+            ["<leader>cR"] = {
+              name = "+Rust",
+              c = { mapping("codeAction"), "Code Action" },
+              o = { mapping("openCargo"), "Open Cargo.toml" },
+              O = { mapping("openDocs"), "Open docs" },
+              r = { mapping("run"), "Run" },
+              R = { mapping("runnables"), "Runnables" },
+            },
+            ["<leader>dr"] = { mapping("debuggables"), "Rust Debuggables" },
+          }, { buffer = bufnr })
+        end,
+      },
+    },
+  },
+}
