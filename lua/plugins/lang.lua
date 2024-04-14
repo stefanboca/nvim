267d304f66f521a22fcdd8ec695b42fa998baf31
Stefan Boca <stefan.r.boca@gmail.com>
2024-04-14 09:58:03 -0700

    refactor

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -50,4 +50,36 @@ return {
       },
     },
   },
+  {
+    "mrcjkb/rustaceanvim",
+    -- dependencies = {
+    --   "folke/which-key.nvim",
+    -- },
+    opts = {
+      server = {
+        on_attach = function(_, bufnr)
+          local wk = require("which-key")
+
+          local function mapping(cmd)
+            return function()
+              vim.cmd.RustLsp(cmd)
+            end
+          end
+
+          wk.register({
+            ["<leader>cR"] = {
+              name = "+Rust",
+              c = { mapping("codeAction"), "Code Action" },
+              o = { mapping("openCargo"), "Open Cargo.toml" },
+              O = { mapping("openDocs"), "Open docs" },
+              r = { mapping("run"), "Run" },
+              R = { mapping("runnables"), "Runnables" },
+              m = { mapping("expandMacro"), "Expand Macro" },
+            },
+            ["<leader>dr"] = { mapping("debuggables"), "Rust Debuggables" },
+          }, { buffer = bufnr })
+        end,
+      },
+    },
+  },
 }
