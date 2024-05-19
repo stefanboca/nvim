6c8a427931b154188b5c88c1419932da03912acc
Stefan Boca <stefan.r.boca@gmail.com>
2024-05-18 17:04:42 -0700

    remove cmp mapping for enter

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -8,7 +8,6 @@ return {
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
         ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
       })
     end,
   },
