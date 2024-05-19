eb0cdaf339aba4e3d25c6c2c1261a9d300c6d903
Stefan Boca <stefan.r.boca@gmail.com>
2024-05-18 17:07:12 -0700

    remove mini-ai extra

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,5 +1,4 @@
 return {
-  { import = "lazyvim.plugins.extras.coding.mini-ai" },
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.dap.core" },
