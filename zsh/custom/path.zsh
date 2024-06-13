975e56999809167961a83e8628782b79e6c9f295
Stefan Boca <stefan.r.boca@gmail.com>
2024-06-13 15:18:00 -0700

    feat(zsh): don't source rye env

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -3,7 +3,7 @@ test -f "$HOME/.cargo/env" && source "$HOME/.cargo/env"
 
 # python rye
 export RYE_HOME="$HOME/.config/rye"
-test -d "$RYE_HOME" && source "$RYE_HOME/env"
+# test -d "$RYE_HOME" && source "$RYE_HOME/env"
 
 # golang
 export GOPATH="$HOME/.local/share/go"
