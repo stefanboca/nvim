be215873259a5a940f06e97286e6fc95373553c3
Stefan Boca <stefan.r.boca@gmail.com>
2024-06-13 16:34:00 -0700

    feat(extras): enable neogen

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,5 +1,6 @@
 return {
   { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  { import = "lazyvim.plugins.extras.coding.neogen" },
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.dap.core" },
   { import = "lazyvim.plugins.extras.editor.dial" },
