6784020c212fcc1a6bebc26a087aaa32f06db79f
Stefan Boca <stefan.r.boca@gmail.com>
2024-06-15 19:58:27 -0700

    simplify rainbow-delimiters config

--- lua/plugins/color.lua
+++ lua/plugins/color.lua
@@ -1,21 +1,5 @@
 return {
   {
     "hiphish/rainbow-delimiters.nvim",
-    config = function(_, opts)
-      require("rainbow-delimiters.setup").setup(opts)
-    end,
-    opts = function(_, opts)
-      local rainbow_delimiters = require("rainbow-delimiters")
-      return {
-        strategy = {
-          [""] = rainbow_delimiters.strategy["global"],
-        },
-        query = {
-          [""] = "rainbow-delimiters",
-          latex = "rainbow-blocks",
-          lua = "rainbow-blocks",
-        },
-      }
-    end,
   },
 }
