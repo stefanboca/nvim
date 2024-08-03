47d68cb90f02ff323488d8c704a661a734c42025
Stefan Boca <stefan.r.boca@gmail.com>
2024-08-03 16:03:51 -0700

    feat(lazyvim): neorg

---
+++ lazyvim/lua/plugins/neorg.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "benlubas/neorg-conceal-wrap",
+    ft = { "norg" },
+    cmd = { "Neorg" },
+  },
+  {
+    "benlubas/neorg-interim-ls",
+    ft = { "norg" },
+    cmd = { "Neorg" },
+  },
+  {
+    "benlubas/neorg-se",
+    ft = { "norg" },
+    cmd = { "Neorg" },
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-neorg/neorg-telescope" },
+      { "benlubas/neorg-conceal-wrap" },
+      { "benlubas/neorg-interim-ls" },
+      { "benlubas/neorg-se" },
+    },
+    ft = { "norg" },
+    cmd = { "Neorg" },
+    opts = {
+      load = {
+        ["core.defaults"] = {
+          disabled = {
+            "core.clipboard",
+            "core.clipboard.code-blocks",
+          },
+        },
+        ["core.concealer"] = {},
+        ["core.completion"] = { config = { engine = { module_name = "external.lsp-completion" } } },
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/data/notes",
+            },
+          },
+        },
+        ["core.integrations.nvim-cmp"] = {},
+        ["core.integrations.telescope"] = {},
+        ["external.conceal-wrap"] = {},
+        ["external.interim-ls"] = {
+          config = {
+            completion_provider = {
+              enable = true,
+              categories = true,
+            },
+          },
+        },
+        ["external.search"] = { config = { index_on_start = true } },
+      },
+    },
+  },
+  {
+    "nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "neorg" })
+    end,
+  },
+}
