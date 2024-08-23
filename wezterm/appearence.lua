65541a88af9c2f53920c6c75b21cc309c0b51037
Stefan Boca <stefan.r.boca@gmail.com>
2024-08-23 09:02:14 -0700

    feat(wezterm): change color scheme and dim inactive panes

--- wezterm/appearence.lua
+++ wezterm/appearence.lua
@@ -3,9 +3,13 @@ local wezterm = require("wezterm")
 local M = {}
 
 function M.apply(config)
-	config.color_scheme = "Catppuccin Mocha"
+	config.color_scheme = "Catppuccin Macchiato"
 	config.font = wezterm.font_with_fallback({ { family = "Fira Code" }, { family = "FiraCode Nerd Font" } })
 	config.font_size = 10
+
+	config.inactive_pane_hsb = {
+		brightness = 0.85,
+	}
 
 	config.use_fancy_tab_bar = false
 	config.tab_bar_at_bottom = true
