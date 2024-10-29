dbee224962a01e219d7d9ab9c4a6f5b290997dbf
Stefan Boca <stefan.r.boca@gmail.com>
2024-10-28 21:39:08 -0700

    feat(nvim): add commands to activate/deactivate otter

--- lazyvim/lua/plugins/otter.lua
+++ lazyvim/lua/plugins/otter.lua
@@ -4,9 +4,17 @@ return {
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
-    event = "LazyFile",
-    opts = {
-      diagnostic_update_events = { "BufWritePost", "InsertLeave", "TextChanged" },
-    },
+    opts = function()
+      vim.api.nvim_create_user_command("OtterActivate", function()
+        require("otter").activate()
+      end, { desc = "Activate Otter for the current buffer" })
+      vim.api.nvim_create_user_command("OtterDeactivate", function()
+        require("otter").deactivate()
+      end, { desc = "Deactivate Otter for the current buffer" })
+
+      return {
+        diagnostic_update_events = { "BufWritePost", "InsertLeave" },
+      }
+    end,
   },
 }
