6886b740f09a817da2a999f123d5f665a8841837
Stefan Boca <stefan.r.boca@gmail.com>
2024-10-28 21:07:11 -0700

    feat(zsh): custom dnf aliases

--- zsh/custom/50-aliases.zsh
+++ zsh/custom/50-aliases.zsh
@@ -5,3 +5,19 @@ alias lg=lazygit
 alias lj=lazyjj
 alias n=nvim
 alias nv=neovide
+
+alias dnfl= "noglob dnf list"                      # List packages
+alias dnfli="noglob dnf list --installed"          # List installed packages
+alias dnfmc="noglob dnf makecache"                 # Generate metadata cache
+alias dnfp= "noglob dnf info"                      # Show package information
+alias dnfs= "noglob dnf search"                    # Search package
+alias dnfrq="noglob dnf repoquery"                 # Repoquery
+
+alias dnfu= "noglob sudo dnf upgrade"              # Upgrade package
+alias dnfi= "noglob sudo dnf install"              # Install package
+alias dnfri="noglob sudo dnf reinstall"            # Reinstall package
+alias dnfr= "noglob sudo dnf remove"               # Remove package
+alias dnfc= "noglob sudo dnf clean all"            # Clean cache
+
+alias dnf= "noglob dnf"
+alias dnf5="noglob dnf5"
