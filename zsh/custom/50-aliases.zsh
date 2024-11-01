6eaf288d1aaa8d5d228573cebf71c805e7f91b9d
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-01 16:33:48 -0700

    feat(zsh): update

--- zsh/custom/50-aliases.zsh
+++ zsh/custom/50-aliases.zsh
@@ -6,18 +6,21 @@ alias lj=lazyjj
 alias n=nvim
 alias nv=neovide
 
-alias dnfl= "noglob dnf list"                      # List packages
+alias dnfl="noglob dnf list"                       # List packages
 alias dnfli="noglob dnf list --installed"          # List installed packages
 alias dnfmc="noglob dnf makecache"                 # Generate metadata cache
-alias dnfp= "noglob dnf info"                      # Show package information
-alias dnfs= "noglob dnf search"                    # Search package
+alias dnfp="noglob dnf info"                       # Show package information
+alias dnfs="noglob dnf search"                     # Search package
 alias dnfrq="noglob dnf repoquery"                 # Repoquery
 
-alias dnfu= "noglob sudo dnf upgrade"              # Upgrade package
-alias dnfi= "noglob sudo dnf install"              # Install package
+alias dnfu="noglob sudo dnf upgrade"               # Upgrade package
+alias dnfi="noglob sudo dnf install"               # Install package
 alias dnfri="noglob sudo dnf reinstall"            # Reinstall package
-alias dnfr= "noglob sudo dnf remove"               # Remove package
-alias dnfc= "noglob sudo dnf clean all"            # Clean cache
+alias dnfsw="noglob sudo dnf swap"                 # Swap packages
+alias dnfr="noglob sudo dnf remove"                # Remove package
+alias dnfc="noglob sudo dnf clean all"             # Clean cache
 
-alias dnf= "noglob dnf"
+alias dnf="noglob dnf"
 alias dnf5="noglob dnf5"
+
+alias rpm="noglob rpm"
