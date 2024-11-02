2593ae1004a0b49c2570de50b47e99040302702a
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-02 12:07:22 -0700

    feat(nvim): temp venv-selector config

---
+++ lazyvim/lua/plugins/venv-selector.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "akthe-at/venv-selector.nvim",
+    branch = "regexp",
+    enabled = true,
+    opts = {
+      options = {
+        picker = "fzf-lua",
+      },
+    },
+  },
+}
