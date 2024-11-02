246fb0411872bd5ae1471ed74bb56fc0adf86638
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-02 12:08:06 -0700

    feat(nvim): update jujutsu plugins

--- lazyvim/lua/plugins/jujutsu.lua
+++ lazyvim/lua/plugins/jujutsu.lua
@@ -10,22 +10,53 @@ function M.wk_desc(buf)
 end
 
 return {
-  "julienvincent/hunk.nvim",
-  cmd = { "DiffEditor" },
-  opts = {
-    keys = {
-      global = {
-        accept = { "<localleader><CR>" },
-        focus_tree = { "<localleader>e" },
+  {
+    "willothy/flatten.nvim",
+    opts = {},
+    -- Ensure that it runs first to minimize delay when opening file from terminal
+    lazy = false,
+    priority = 1001,
+  },
+
+  {
+    "julienvincent/hunk.nvim",
+    cmd = { "DiffEditor" },
+    opts = {
+      keys = {
+        global = {
+          accept = { "<localleader><CR>" },
+          focus_tree = { "<localleader>e" },
+        },
+      },
+      hooks = {
+        on_tree_mount = function(context)
+          M.wk_desc(context.buf)
+        end,
+        on_diff_mount = function(context)
+          M.wk_desc(context.buf)
+        end,
       },
     },
-    hooks = {
-      on_tree_mount = function(context)
-        M.wk_desc(context.buf)
-      end,
-      on_diff_mount = function(context)
-        M.wk_desc(context.buf)
-      end,
+  },
+
+  {
+    "avm99963/vim-jjdescription",
+    ft = { "jj", "jjdescription" },
+  },
+
+  {
+    "Cretezy/neo-tree-jj.nvim",
+    enabled = false,
+    dependencies = {
+      {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = function(_, opts)
+          if require("neo-tree.sources.jj.utils").get_repository_root() then
+            -- Register the source
+            table.insert(opts.sources, "jj")
+          end
+        end,
+      },
     },
   },
 }
