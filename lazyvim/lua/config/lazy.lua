9037679655941bf2102c6576b78f4cd3c6203563
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-01 18:14:32 -0700

    refactor(nvim): clean up lazy.lua

--- lazyvim/lua/config/lazy.lua
+++ lazyvim/lua/config/lazy.lua
@@ -15,35 +15,25 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  spec = {
-    -- add LazyVim and import its plugins
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "catppuccin",
-      },
-      import = "lazyvim.plugins",
-    },
-    { import = "plugins" },
+  -- add LazyVim and import its plugins
+  {
+    "LazyVim/LazyVim",
+    dev = true,
+    opts = { colorscheme = "catppuccin" },
+    import = "lazyvim.plugins",
   },
+  -- import custom plugins
+  { import = "plugins" },
+}, {
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
     lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  rocks = { hererocks = true },
   dev = {
     path = "~/data/plugins",
   },
   install = { colorscheme = { "catppuccin", "tokyonight", "habamax" } },
-  checker = {
-    enabled = false, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+  checker = { enabled = false },
   performance = {
     rtp = {
       -- disable some rtp plugins
