e69c6b285287a647ebfccb6337e9fdb6e4634363
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-05 14:51:31 -0800

    feat(nvim): bookmarks(?)

---
+++ lazyvim/lua/plugins/bookmarks.lua
@@ -0,0 +1,17 @@
+return {
+  -- {
+  --   "EvWilson/spelunk.nvim",
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     -- "nvim-telescope/telescope.nvim", -- Optional: for fuzzy search capabilities
+  --   },
+  --   opts = {
+  --     enable_persist = true,
+  --   },
+  -- },
+
+  -- {
+  --   "tristone13th/lspmark.nvim",
+  --   opts = {},
+  -- },
+}
