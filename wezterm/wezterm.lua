6399052a9f4a018b019cfda757f4f415aef5b45f
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-07 17:36:49 -0800

    feat(wezterm): update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -37,9 +37,18 @@ config.window_padding = {
 }
 config.default_cursor_style = "SteadyBlock"
 
+-- Workspace
 local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
 workspace_switcher.zoxide_path = "/home/doctorwho/.cargo/bin/zoxide"
 config.default_workspace = "~"
+
+-- config.unix_domains = { { name = "unix" } }
+
+-- local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+-- wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+-- 	local workspace_state = resurrect.workspace_state
+-- 	resurrect.save_state(workspace_state.get_workspace_state())
+-- end)
 
 -- Keys
 local act = wezterm.action
@@ -59,7 +68,7 @@ config.keys = {
 	{ key = "q", mods = "LEADER", action = act.PaneSelect },
 
 	-- Pane Manipulation
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "m", mods = "LEADER", action = act.TogglePaneZoomState },
 	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
 	{ key = "o", mods = "LEADER|CTRL", action = act.RotatePanes("CounterClockwise") },
 	{ key = "o", mods = "LEADER|ALT", action = act.RotatePanes("Clockwise") },
@@ -84,8 +93,47 @@ config.keys = {
 	{ key = "[", mods = "LEADER", action = act.ActivateCopyMode },
 	{ key = "]", mods = "LEADER", action = act.PasteFrom("Clipboard") },
 
-	-- smart_workspace_switcher
-	{ key = "s", mods = "LEADER", action = workspace_switcher.switch_workspace() },
+	-- Workspace
+	{ key = "z", mods = "LEADER", action = workspace_switcher.switch_workspace() },
+
+	-- {
+	-- 	key = "s",
+	-- 	mods = "LEADER",
+	-- 	action = wezterm.action_callback(function(win, pane)
+	-- 		resurrect.save_state(resurrect.workspace_state.get_workspace_state())
+	-- 		resurrect.window_state.save_window_action()
+	-- 	end),
+	-- },
+	-- {
+	-- 	key = "r",
+	-- 	mods = "LEADER",
+	-- 	action = wezterm.action_callback(function(win, pane)
+	-- 		resurrect.fuzzy_load(win, pane, function(id, label)
+	-- 			local type = string.match(id, "^([^/]+)") -- match before '/'
+	-- 			id = string.match(id, "([^/]+)$") -- match after '/'
+	-- 			id = string.match(id, "(.+)%..+$") -- remove file extention
+	-- 			local opts = {
+	-- 				relative = true,
+	-- 				restore_text = true,
+	-- 				on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+	-- 			}
+	-- 			if type == "workspace" then
+	-- 				local state = resurrect.load_state(id, "workspace")
+	-- 				resurrect.workspace_state.restore_workspace(state, opts)
+	-- 			elseif type == "window" then
+	-- 				local state = resurrect.load_state(id, "window")
+	-- 				resurrect.window_state.restore_window(pane:window(), state, opts)
+	-- 			elseif type == "tab" then
+	-- 				local state = resurrect.load_state(id, "tab")
+	-- 				resurrect.tab_state.restore_tab(pane:tab(), state, opts)
+	-- 			end
+	-- 		end)
+	-- 	end),
+	-- },
+
+	-- Domain
+	{ key = "a", mods = "LEADER", action = act.AttachDomain("unix") },
+	{ key = "d", mods = "LEADER", action = act.DetachDomain({ DomainName = "unix" }) },
 }
 for i = 1, 9 do
 	-- <LEADER-i> to activate that tab
