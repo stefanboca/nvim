ce65207d2198fbcbaae8eaa9979685a4e033293d
Stefan Boca <stefan.r.boca@gmail.com>
2024-11-24 11:02:59 -0800

    feat(nvim): tiny inline diagnostics

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,20 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- Remap diagnostic jumps to not open float
+local diagnostic_goto = function(next, severity)
+  local count = next and 1 or -1
+  severity = severity and vim.diagnostic.severity[severity] or nil
+  return function()
+    vim.diagnostic.jump({ count = count, severity = severity, float = false })
+  end
+end
+map("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+map("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+map("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+map("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+map("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+map("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
--- lazyvim/lua/plugins/lsp.lua
+++ lazyvim/lua/plugins/lsp.lua
@@ -10,7 +10,33 @@ return {
           focusable = true,
           source = "if_many",
         },
+        virtual_text = false,
       },
     },
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    opts = {
+      options = {
+        -- softwrap = 20,
+        multiple_diag_under_cursor = true,
+        multilines = true,
+        show_all_diags_on_cursorline = true,
+        enable_on_insert = true,
+      },
+    },
+    config = function(_, opts)
+      local diags = require("tiny-inline-diagnostic.diagnostic")
+      Snacks.toggle({
+        name = "Diagnostics",
+        get = function()
+          return diags.enabled
+        end,
+        set = diags.toggle,
+      }):map("<leader>ud")
+      require("tiny-inline-diagnostic").setup(opts)
+    end,
   },
 }
