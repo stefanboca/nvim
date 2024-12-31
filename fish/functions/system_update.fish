d1133a6e8a6f234432a01ce4fd7b939444a748ab
Stefan Boca <stefan.r.boca@gmail.com>
2024-12-30 23:14:21 -0800

    fish: fix system update function

--- fish/functions/system_update.fish
+++ fish/functions/system_update.fish
@@ -1,28 +1,33 @@
 function __system_update_run
-    set -f cmd $argv[1]
+    argparse "t/test-cmd=" q/quiet -- $argv
+    set -l cmd $argv
 
-    set -f test_cmd $argv[1]
-    if set -q argv[2]
-        set test_cmd $argv[2]
+    if not set -q _flag_test_cmd
+        set _flag_test_cmd $cmd[1]
     end
 
-    if command -q $test_cmd
+    if type -q $_flag_test_cmd
         echo (set_color --bold cyan)"Running `$cmd`..."(set_color normal)
 
-        eval $cmd
-        and echo (set_color --bold cyan)"Done running `$cmd`."(set_color normal)
-        or echo (set_color --bold red)"Error running `$cmd`."(set_color normal)
+        if set -q _flag_quiet
+            $cmd >/dev/null
+        else
+            $cmd
+        end
+        and echo (set_color --bold green)"Done running `$cmd`."
+        or echo (set_color --bold red)"Error running `$cmd`."
+        set_color normal
     end
 end
 
 function system_update --description "Update all the things"
-    __system_update_run "sudo dnf upgrade --refresh" dnf
-    __system_update_run "flatpak update"
-    __system_update_run "fisher update"
-    __system_update_run "uv self update"
-    __system_update_run "uv tool upgrade --all"
-    __system_update_run "rustup self update"
-    __system_update_run "rustup update"
-    __system_update_run "cargo install-update -a" cargo-install-update
-    __system_update_run "tldr --update"
+    __system_update_run -t dnf -- sudo dnf upgrade --refresh
+    __system_update_run flatpak update
+    __system_update_run -q -- fisher update
+    __system_update_run uv self update
+    __system_update_run uv tool upgrade --all
+    __system_update_run rustup self update
+    __system_update_run rustup update
+    __system_update_run -t cargo-install-update -- cargo install-update -a
+    __system_update_run tldr --update
 end
