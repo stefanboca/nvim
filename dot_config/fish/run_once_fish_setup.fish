7ecfdcafe8b3872b0d2bb51351d8b295c99ba3f8
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-01 22:55:07 -0800

    fish: cleanup with chezmoi

--- dot_config/fish/run_once_fish_setup.fish
+++ dot_config/fish/run_once_fish_setup.fish
@@ -1,5 +1,14 @@
 #!/bin/env fish
 
+echo y | fish_config theme save tokyonight_storm
+
+# install fisher plugins
+if not test -f $fisher_path
+    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+end
+fisher update
+
+# setup tide prompt
 tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Solid --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
 if not contains jj $tide_right_prompt_items
     set -Ua tide_right_prompt_items jj
--- dot_config/fish/run_once_fisher_install.fish
+++
@@ -1,7 +0,0 @@
-#!/bin/env fish
-
-if not test -f $fisher_path
-    curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
-end
-
-fisher update
