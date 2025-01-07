cc960f32ce93802d08b61b55307a674d78cf5f12
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-06 16:23:36 -0800

    fix: fish setup script

--- dot_config/fish/functions/_tide_item_git_disable_if_jj.fish
+++ dot_config/fish/functions/_tide_item_git_disable_if_jj.fish
@@ -1,4 +1,4 @@
-function _tide_item_git_no_jj
+function _tide_item_git_disable_if_jj
     command -q jj && jj --ignore-working-copy root &>/dev/null && return 1
 
     _tide_item_git
--- dot_config/fish/run_once_after_fish_setup.fish
+++ dot_config/fish/run_once_after_fish_setup.fish
@@ -1,17 +1,17 @@
 #!/bin/env fish
 
+# disable greeting
 set -U fish_greeting
 
+# set theme
 echo y | fish_config theme save tokyonight_storm
 
 # install fisher plugins
-if not test -f $fisher_path
+if not test -d $fisher_path
     curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source
+    fisher update
 end
-fisher update
 
 # setup tide prompt
 tide configure --auto --style=Lean --prompt_colors='True color' --show_time=No --lean_prompt_height='Two lines' --prompt_connection=Solid --prompt_connection_andor_frame_color=Darkest --prompt_spacing=Sparse --icons='Few icons' --transient=Yes
-if not contains jj $tide_right_prompt_items
-    set -Ua tide_right_prompt_items jj
-end
+set -U tide_left_prompt_items pwd git_disable_if_jj jj newline character
