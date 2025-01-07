702d18afdcbbd82f0a9f285aa8552930d71a2393
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-06 22:38:52 -0800

    refactor(fish): rework tide jj color printing using labels

--- dot_config/fish/functions/_tide_item_jj.fish
+++ dot_config/fish/functions/_tide_item_jj.fish
@@ -1,35 +1,29 @@
 function _tide_item_jj
     command -q jj && jj --ignore-working-copy root &>/dev/null || return 1
 
-    set -q tide_jj_description_color && set description_color (set_color $tide_jj_description_color | string escape)
-    set -q tide_jj_conflict_color && set conflict_color (set_color $tide_jj_conflict_color | string escape)
-    set -q tide_jj_divergent_color && set divergent_color (set_color $tide_jj_divergent_color | string escape)
-    set -q tide_jj_hidden_color && set hidden_color (set_color $tide_jj_hidden_color | string escape)
-
     _tide_print_item jj $tide_jj_icon' ' (
-        jj log -r@ --ignore-working-copy --no-pager --no-graph --color always -T "
-        separate(' ',
+        jj log -r@ --ignore-working-copy --no-pager --no-graph --color always -T '
+        separate(" ",
             change_id.shortest(8),
             commit_id.shortest(8),
             bookmarks.map(|x| if(
                 x.name().substr(0, 10).len() <= 10,
                 x.name(),
-                x.name().substr(0, 9) ++ '…')
-            ).join(' '),
+                x.name().substr(0, 9) ++ "…")
+            ).join(" "),
             tags.map(|x| if(
                 x.name().substr(0, 10).len() <= 10,
                 x.name(),
-                x.name().substr(0, 9) ++ '…')
-            ).join(' '),
-            surround('$description_color', '', if(
+                x.name().substr(0, 9) ++ "…")
+            ).join(" "),
+            if(
                 description.first_line().len() <= 24,
                 description.first_line(),
-                description.first_line().substr(0, 23) ++ '…'
-            )),
-            if(conflict, '""$conflict_color""conflict'),
-            if(divergent, '""$divergent_color""divergent'),
-            if(hidden, '""$hidden_color""hidden'),
-        )" | string unescape;
-        set_color normal
-        )
+                description.first_line().substr(0, 23) ++ "…"
+            ),
+            label("conflict", if(conflict, "conflict")),
+            label("divergent", if(divergent, "divergent")),
+            label("hidden", if(hidden, "hidden")),
+        )'
+    )
 end
