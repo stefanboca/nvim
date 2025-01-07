d83b4ec8ba587ff7d12390f4b2bbd529c507d110
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-07 14:44:53 -0800

    feat(nvim.blink): buffer: keep first letter capitalization

--- dot_config/nvim/lua/plugins/extras/coding/blink.lua
+++ dot_config/nvim/lua/plugins/extras/coding/blink.lua
@@ -70,6 +70,42 @@ return {
           end
           return {}
         end,
+
+        providers = {
+          buffer = {
+            -- Keep first letter capitalization
+            transform_items = function(a, items)
+              local keyword = a.get_keyword()
+              local correct, case
+              if keyword:match("^%l") then
+                correct = "^%u%l+$"
+                case = string.lower
+              elseif keyword:match("^%u") then
+                correct = "^%l+$"
+                case = string.upper
+              else
+                return items
+              end
+
+              -- avoid duplicates from the corrections
+              local seen = {}
+              local out = {}
+              for _, item in ipairs(items) do
+                local raw = item.insertText
+                if raw:match(correct) then
+                  local text = case(raw:sub(1, 1)) .. raw:sub(2)
+                  item.insertText = text
+                  item.label = text
+                end
+                if not seen[item.insertText] then
+                  seen[item.insertText] = true
+                  table.insert(out, item)
+                end
+              end
+              return out
+            end,
+          },
+        },
       },
     },
     spec = {
