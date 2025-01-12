c9af53fb17e99176e1b3531203f148939662fd77
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-11 22:18:29 -0800

    feat(nvim): auto_show for multiline diagnostics

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -104,7 +104,10 @@ return {
     opts = {
       options = {
         multiple_diag_under_cursor = true,
-        multilines = true,
+        multilines = {
+          enabled = true,
+          always_show = true,
+        },
         show_all_diags_on_cursorline = true,
         enable_on_insert = true,
       },
