e207d2e84180cbaa6721ddd36ca909e445a69d5e
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-14 20:32:30 -0800

    feat(nvim): typst sync cursor map

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,20 +6,33 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("custom_" .. name, { clear = true })
 end
 
--- add autopairs for typst and enable spelling
+-- TODO: why doesn't FileType fire when opening files from the cli?
+
+-- Typst config
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("typst"),
   pattern = { "typst" },
   callback = function(event)
     local bufnr = event.buf
+
+    -- enable spelling
     vim.bo[bufnr].textwidth = 80
     vim.opt_local.spell = true
 
+    -- add autopairs
     local pairs = require("mini.pairs")
     pairs.map_buf(bufnr, "i", " ", { action = "open", pair = "  ", neigh_pattern = "%$%$" })
     pairs.map_buf(bufnr, "i", "$", { action = "closeopen", pair = "$$" })
     pairs.map_buf(bufnr, "i", "`", { action = "closeopen", pair = "``" })
     -- * and _ are not autopaired because they can be used in math equations
+
+    vim.api.nvim_buf_set_keymap(
+      bufnr,
+      "n",
+      "<localleader>s",
+      "<cmd>TypstPreviewSyncCursor<cr>",
+      { desc = "Typst Preview Sync Cursor" }
+    )
   end,
 })
 
