562154a2249eddddccde722c8fe58218ce5871b1
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-23 09:15:10 -0800

    nvim: todo-comment safety and invariant

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -7,4 +7,14 @@ return {
       vim.g.nvim_ghost_autostart = 0
     end,
   },
+
+  {
+    "todo-comments.nvim",
+    opts = {
+      keywords = {
+        SAFETY = { color = "warning", alt = { "safety", "Safety" } },
+        INVARIANT = { color = "hint", alt = { "invariant", "Invariant" } },
+      },
+    },
+  },
 }
