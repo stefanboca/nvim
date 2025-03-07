c8592bde489084e6cbae02d447aace83861d7c7b
Stefan Boca <stefan.r.boca@gmail.com>
2025-03-06 21:29:06 -0800

    fish: specify env vars to move more files out of home
    found with xdg-ninja

--- dot_config/fish/conf.d/00-env.fish
+++ dot_config/fish/conf.d/00-env.fish
@@ -10,6 +10,9 @@ set -gx NPM_CONFIG_INIT_MODULE $XDG_CONFIG_HOME/npm/config/npm-init.js
 set -gx NPM_CONFIG_CACHE $XDG_CACHE_HOME/npm
 set -gx NPM_CONFIG_TMP $XDG_RUNTIME_DIR/npm
 set -gx PYTHON_HISTORY $XDG_STATE_HOME/python_history
+set -gx SQLITE_HISTORY $XDG_CACHE_HOME/sqlite_history
+set -gx WINEPREFIX $XDG_DATA_HOME/wine
+set -gx CUDA_CACHE_PATH $XDG_CACHE_HOME/nv
 
 set --path -gx TERMINFO_DIRS $XDG_DATA_HOME/terminfo /usr/share/terminfo
 
