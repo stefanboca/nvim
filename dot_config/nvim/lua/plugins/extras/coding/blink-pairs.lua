4b6aee41f58cba2ed4918f5a3eeb21c199944969
Stefan Boca <stefan.r.boca@gmail.com>
2025-03-06 21:29:06 -0800

    nvim: blink.pairs

---
+++ dot_config/nvim/lua/plugins/extras/coding/blink-pairs.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "mini.pairs",
+    enabled = false,
+  },
+
+  {
+    "saghen/blink.pairs",
+    build = "cargo build --release",
+
+    --- @module 'blink.pairs'
+    --- @type blink.pairs.Config
+    opts = {
+      mappings = {
+        enabled = true,
+      },
+      highlights = {
+        enabled = true,
+        groups = {
+          "RainbowDelimiterRed",
+          "RainbowDelimiterOrange",
+          "RainbowDelimiterYellow",
+          "RainbowDelimiterGreen",
+          "RainbowDelimiterCyan",
+          "RainbowDelimiterBlue",
+          "RainbowDelimiterViolet",
+        },
+      },
+    },
+  },
+
+  {
+    "catpuccin/nvim",
+    optional = true,
+    opts = {
+      integrations = {
+        rainbow_delimiters = true,
+      },
+    },
+  },
+}
