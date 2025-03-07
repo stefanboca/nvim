cdd8e5666c598995cf39a5e0a851edc32971df0f
Stefan Boca <stefan.r.boca@gmail.com>
2025-03-06 21:29:06 -0800

    nvim: fix render-markdown

--- dot_config/nvim/lua/langs/markdown.lua
+++ dot_config/nvim/lua/langs/markdown.lua
@@ -69,13 +69,39 @@ return {
   },
 
   {
-    "render-markdown.nvim",
+    "MeanderingProgrammer/render-markdown.nvim",
     opts = {
       completions = {
-        lsp = {
-          enabled = true,
-        },
+        lsp = { enabled = true },
+      },
+      code = {
+        sign = false,
+        width = "block",
+        right_pad = 1,
+      },
+      heading = {
+        sign = false,
+        icons = {},
+      },
+      checkbox = {
+        enabled = false,
       },
     },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
+    config = function(_, opts)
+      require("render-markdown").setup(opts)
+      Snacks.toggle({
+        name = "Render Markdown",
+        get = function() return require("render-markdown.state").enabled end,
+        set = function(enabled)
+          local m = require("render-markdown")
+          if enabled then
+            m.enable()
+          else
+            m.disable()
+          end
+        end,
+      }):map("<leader>um")
+    end,
   },
 }
