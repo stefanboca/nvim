4551a358dc87c3638600e0810c52151121f24426
Stefan Boca <stefan.r.boca@gmail.com>
2025-03-31 10:20:04 -0700

    nvim: mini.files

--- dot_config/nvim/lua/core/ui.lua
+++ dot_config/nvim/lua/core/ui.lua
@@ -166,12 +166,9 @@ return {
   {
     "snacks.nvim",
     keys = {
-      { "<leader>fe", function() Snacks.explorer() end, desc = "Explorer Snacks" },
-      { "<leader>e", "<leader>fe", desc = "Explorer Snacks", remap = true },
       { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
     },
     opts = {
-      explorer = { enabled = true },
       indent = { enabled = true },
       input = { enabled = true },
       notifier = { enabled = true },
@@ -202,5 +199,94 @@ return {
         },
       },
     },
+  },
+
+  {
+    "echasnovski/mini.files",
+    lazy = vim.fn.argc(-1) == 0, -- load early when opening a directory from cmdline
+    opts = {
+      options = {
+        use_as_default_explorer = true,
+      },
+      windows = {
+        preview = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          if not MiniFiles.close() then MiniFiles.open() end
+        end,
+        desc = "File Explorer",
+      },
+      {
+        "<leader>fE",
+        function()
+          if not MiniFiles.close() then MiniFiles.open(vim.api.nvim_buf_get_name(0)) end
+        end,
+        desc = "File Explorer (Current File)",
+      },
+      { "<leader>e", "<leader>fe", remap = true, desc = "File Explorer" },
+      { "<leader>E", "<leader>fE", remap = true, desc = "File Explorer (Current File)" },
+    },
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+
+      local show_dotfiles = true
+      local filter_show = function() return true end
+      local filter_hide = function(fs_entry) return not vim.startswith(fs_entry.name, ".") end
+
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        MiniFiles.refresh({ content = { filter = show_dotfiles and filter_show or filter_hide } })
+      end
+
+      local files_set_cwd = function()
+        local cur_entry_path = MiniFiles.get_fs_entry().path
+        local cur_directory = vim.fs.dirname(cur_entry_path)
+        if cur_directory ~= nil then vim.fn.chdir(cur_directory) end
+      end
+
+      local map_split = function(buf_id, lhs, direction, close_on_file)
+        local rhs = function()
+          local new_target_window
+          local cur_target_window = require("mini.files").get_explorer_state().target_window
+          if cur_target_window ~= nil then
+            vim.api.nvim_win_call(cur_target_window, function()
+              vim.cmd("belowright " .. direction .. " split")
+              new_target_window = vim.api.nvim_get_current_win()
+            end)
+
+            require("mini.files").set_target_window(new_target_window)
+            require("mini.files").go_in({ close_on_file = close_on_file })
+          end
+        end
+
+        local desc = "Open in " .. direction .. " split"
+        if close_on_file then desc = desc .. " and close" end
+        vim.keymap.set("n", lhs, rhs, { buffer = buf_id, desc = desc })
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+
+          vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id, desc = "Toggle hidden files" })
+          vim.keymap.set("n", "gc", files_set_cwd, { buffer = buf_id, desc = "Set CWD" })
+
+          map_split(buf_id, "<C-w>s", "horizontal", false)
+          map_split(buf_id, "<C-w>v", "vertical", false)
+          map_split(buf_id, "<C-w>S", "horizontal", true)
+          map_split(buf_id, "<C-w>V", "vertical", true)
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesActionRename",
+        callback = function(event) Snacks.rename.on_rename_file(event.data.from, event.data.to) end,
+      })
+    end,
   },
 }
