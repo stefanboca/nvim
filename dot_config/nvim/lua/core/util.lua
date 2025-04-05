3537c663df6bc62ebc844e3fb3b2aed4fa3a6e29
Stefan Boca <stefan.r.boca@gmail.com>
2025-04-04 23:13:43 -0700

    nvim: scratch buffer improvements

--- dot_config/nvim/lua/core/util.lua
+++ dot_config/nvim/lua/core/util.lua
@@ -27,7 +27,11 @@ return {
       },
     },
     keys = {
-      { "<leader>.", function() Snacks.scratch() end, desc = "Toggle Scratch Buffer" },
+      {
+        "<leader>.",
+        function() Snacks.scratch({ ft = "lua", win = { width = 0.6, height = 0.6, relative = "editor" } }) end,
+        desc = "Toggle Scratch Buffer (lua)",
+      },
       { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
       { "<leader>dps", function() Snacks.profiler.scratch() end, desc = "Profiler Scratch Buffer" },
     },
