7937f3320a5fdf61b092d9af0ecf88535e833a1b
Stefan Boca <stefan.r.boca@gmail.com>
2025-05-23 14:19:19 -0700

    feat: clean up ty
    filetypes and root_markers now come from lspconfig.

--- lsp/ty.lua
+++ lsp/ty.lua
@@ -1,15 +1,6 @@
 ---@type vim.lsp.Config
 return {
   cmd = { vim.env.HOME .. "/data/software/utils/ruff/target/release/ty", "server" },
-  filetypes = { "python" },
-  root_marksrs = {
-    "pyproject.toml",
-    "setup.py",
-    "setup.cfg",
-    "requirements.txt",
-    "Pipfile",
-    "pyrightconfig.json",
-  },
   settings = {
     experimental = {
       completions = {
