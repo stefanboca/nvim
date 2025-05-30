bbbea6570e44a6da102ff404c4b6c1b25c768c5d
Stefan Boca <stefan.r.boca@gmail.com>
2025-05-30 10:31:48 -0700

    perf: remove part of the lua @language injection query
    it was really slow

--- queries/lua/injections.scm
+++ queries/lua/injections.scm
@@ -6,11 +6,6 @@
   content: (_) @injection.language
   (#gsub! @injection.language "^[-][%s]*@language[%s]+(%S+)" "%1"))
   .
-  ; allow type annotations between @language and variable declaration
-  (comment
-    content: (_) @_content
-    (#lua-match? @_content "^[-][%s]*[@|]"))*
-  .
   (variable_declaration
     (assignment_statement
       (expression_list
