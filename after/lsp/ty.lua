0ac5e449c57e7ca0db0883e5d2e522e96319fe79
Stefan Boca <stefan.r.boca@gmail.com>
2025-06-15 13:45:42 -0700

    feat: clean up ty config
    my path changed, and completions are now enabled by default

--- after/lsp/ty.lua
+++ after/lsp/ty.lua
@@ -1,11 +1,4 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { vim.env.HOME .. "/data/software/utils/ruff/target/release/ty", "server" },
-  settings = {
-    experimental = {
-      completions = {
-        enable = true,
-      },
-    },
-  },
+  cmd = { "~/src/3rd/ruff/target/release/ty", "server" },
 }
