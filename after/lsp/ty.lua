5d95954830a586c4d06f5761f2fd1bce22271a5d
Stefan Boca <stefan.r.boca@gmail.com>
2025-06-23 20:46:43 -0700

    feat(ty): settings

--- after/lsp/ty.lua
+++ after/lsp/ty.lua
@@ -1,4 +1,15 @@
 ---@type vim.lsp.Config
 return {
   cmd = { vim.env.HOME .. "/src/3rd/ruff/target/release/ty", "server" },
+  settings = {
+    ty = {
+      inlayHints = {
+        variableTypes = true,
+        functionArgumentNames = true,
+      },
+      experimental = {
+        rename = true,
+      },
+    },
+  },
 }
