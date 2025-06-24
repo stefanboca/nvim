17674000dc0feac6d589ebd037d61164211a70d0
Stefan Boca <stefan.r.boca@gmail.com>
2025-06-23 20:46:43 -0700

    feat: justfile formatter

--- lua/my/langs/just.lua
+++ lua/my/langs/just.lua
@@ -3,4 +3,13 @@ return {
     "neovim/nvim-lspconfig",
     opts = { enabled = { "just" } },
   },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        ["just"] = { "just" },
+      },
+    },
+  },
 }
