3065955b11899f34419a872f85ff29350ce9264c
Stefan Boca <stefan.r.boca@gmail.com>
2025-06-23 20:46:43 -0700

    chore(ty): remove now-default settings

--- after/lsp/ty.lua
+++ after/lsp/ty.lua
@@ -3,10 +3,6 @@ return {
   cmd = { vim.env.HOME .. "/src/3rd/ruff/target/release/ty", "server" },
   settings = {
     ty = {
-      inlayHints = {
-        variableTypes = true,
-        functionArgumentNames = true,
-      },
       experimental = {
         rename = true,
       },
