e80b95d93a4f1a638e63bc6b24b815004aa92158
Stefan Boca <stefan.r.boca@gmail.com>
2025-08-16 12:28:36 -0700

    feat(lean): treesitter

--- lua/my/langs/lean.lua
+++ lua/my/langs/lean.lua
@@ -1,4 +1,22 @@
+vim.api.nvim_create_autocmd("User", {
+  pattern = "TSUpdate",
+  callback = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require("nvim-treesitter.parsers").lean = {
+      ---@diagnostic disable-next-line: missing-fields
+      install_info = {
+        url = "https://github.com/Julian/tree-sitter-lean",
+      },
+    }
+  end,
+})
+
 return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { enabled = { "lean" } },
+  },
+
   {
     "Julian/lean.nvim",
     event = { "BufReadPre *.lean", "BufNewFile *.lean" },
