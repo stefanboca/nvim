4b8e85fdf04a7f87d8217e0003744dc56ba49d4c
Stefan Boca <stefan.r.boca@gmail.com>
2025-08-16 12:28:36 -0700

    fix: deprecated function

--- lua/my/core/lsp.lua
+++ lua/my/core/lsp.lua
@@ -85,7 +85,7 @@ return {
         end,
       })
 
-      vim.lsp.set_log_level(vim.env.NVIM_LSP_DEBUG ~= nil and vim.log.levels.TRACE or vim.log.levels.OFF)
+      vim.lsp.log.set_level(vim.env.NVIM_LSP_DEBUG ~= nil and vim.log.levels.TRACE or vim.log.levels.OFF)
       vim.lsp.enable(opts.enabled or {})
       vim.lsp.inlay_hint.enable()
     end,
