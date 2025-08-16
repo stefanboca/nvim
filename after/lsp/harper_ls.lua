81f482cf95723e95f0d2047bc837021c1c92eb56
Stefan Boca <stefan.r.boca@gmail.com>
2025-08-16 12:28:36 -0700

    feat: harper ignore long sentences

--- after/lsp/harper_ls.lua
+++ after/lsp/harper_ls.lua
@@ -1,9 +1,9 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { "harper-ls", "--stdio", "--skip-version-check" },
   settings = {
     ["harper-ls"] = {
       linters = {
+        LongSentences = false,
         SentenceCapitalization = false,
         SpellCheck = false,
         ToDoHyphen = false,
