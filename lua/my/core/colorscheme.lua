0e655c5c093a05b2317578c658387a40ede35da6
Stefan Boca <stefan.r.boca@gmail.com>
2025-08-16 12:28:36 -0700

    feat(colorscheme): invert comment fg/bg

--- lua/my/core/colorscheme.lua
+++ lua/my/core/colorscheme.lua
@@ -23,6 +23,11 @@ return {
       custom_highlights = function(c)
         local hls = {
           MatchParen = { fg = "NONE", bg = c.surface1, style = { "bold" } },
+          ["@comment.error"] = { fg = c.red, bg = "NONE" },
+          ["@comment.warning"] = { fg = c.yellow, bg = "NONE" },
+          ["@comment.hint"] = { fg = c.blue, bg = "NONE" },
+          ["@comment.todo"] = { fg = c.flamingo, bg = "NONE" },
+          ["@comment.note"] = { fg = c.rosewater, bg = "NONE" },
         }
 
         local function pairs_hl(color, fg)
