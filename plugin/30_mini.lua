b99886509fb663bb3e97626b1560fa69e606f7a6
Stefan Boca <stefan.r.boca@gmail.com>
2025-10-18 23:02:33 -0700

    feat(notify): filter out some lua_ls progress
    see: https://github.com/echasnovski/nvim/blob/9d9b6c057aa6f4731efd4e5df3ecf350bdba6730/plugin/30_mini.lua#L35-L40

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -29,7 +29,15 @@ now_if_args(function()
   MiniMisc.setup_restore_cursor()
 end)
 
-now(function() require("mini.notify").setup() end)
+now(function()
+  local function predicate(notif)
+    if not (notif.data.source == "lsp_progress" and notif.data.client_name == "lua_ls") then return true end
+    -- Filter out some LSP progress notifications from 'lua_ls'
+    return notif.msg:find("Diagnosing") == nil and notif.msg:find("semantic tokens") == nil
+  end
+  local custom_sort = function(notif_arr) return MiniNotify.default_sort(vim.tbl_filter(predicate, notif_arr)) end
+  require("mini.notify").setup({ content = { sort = custom_sort } })
+end)
 
 now(function() require("mini.sessions").setup() end)
 
