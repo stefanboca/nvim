600d24e29314933e58b6bbe5ae09d949ab7335f4
Stefan Boca <stefan.r.boca@gmail.com>
2025-10-21 10:22:32 -0700

    feat(lua_ls): update config

--- after/lsp/lua_ls.lua
+++ after/lsp/lua_ls.lua
@@ -13,17 +13,7 @@ return {
       completion = {
         callSnippet = "Replace",
       },
-      doc = {
-        privateName = { "^_" },
-      },
-      hint = {
-        enable = true,
-        setType = false,
-        paramType = true,
-        paramName = "Disable",
-        semicolon = "Disable",
-        arrayIndex = "Disable",
-      },
+      telemetry = { enable = false },
     },
   },
 }
