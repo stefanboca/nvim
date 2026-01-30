7a6e8530246d72391ae04c869601be56afc43254
stefan <stefan.r.boca@gmail.com>
2026-01-30 13:24:02 -0800

    rust: override default textwidth=100

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,3 +1,5 @@
+vim.opt_local.textwidth = 0
+
 MiniDeps.later(function()
   ---@diagnostic disable-next-line: missing-fields
   require("neotest").setup({ adapters = { require("rustaceanvim.neotest") } })
