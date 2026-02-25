126f4ba325513ca55a99d3788507e0dde4bb6187
stefan <stefan.r.boca@gmail.com>
2026-02-24 21:37:38 -0800

    fix dap adapter loading

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -4,13 +4,15 @@ local packadd = vim.cmd.packadd
 nmapb_lleader("c", '<Cmd>lua require("dap-python").test_class()<CR>', "Debug Class")
 nmapb_lleader("t", '<Cmd>lua require("dap-python").test_method()<CR>', "Debug Method")
 
-if not vim.g.nvim_dap_python_init then
-  packadd("nvim-dap-python")
-  require("dap-python").setup("uv")
+MiniDeps.later(function()
+  if not vim.g.nvim_dap_python_init then
+    packadd("nvim-dap-python")
+    require("dap-python").setup("uv")
 
-  packadd("neotest-python")
-  ---@diagnostic disable-next-line: missing-fields
-  require("neotest").setup({ adapters = { require("neotest-python") } })
+    packadd("neotest-python")
+    ---@diagnostic disable-next-line: missing-fields
+    require("neotest").setup({ adapters = { require("neotest-python") } })
 
-  vim.g.nvim_dap_python_init = true
-end
+    vim.g.nvim_dap_python_init = true
+  end
+end)
--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -1,8 +1,10 @@
 vim.opt_local.textwidth = 0
 
-if not vim.g.neotest_rust_init then
-  ---@diagnostic disable-next-line: missing-fields
-  require("neotest").setup({ adapters = { require("rustaceanvim.neotest") } })
+MiniDeps.later(function()
+  if not vim.g.neotest_rust_init then
+    ---@diagnostic disable-next-line: missing-fields
+    require("neotest").setup({ adapters = { require("rustaceanvim.neotest") } })
 
-  vim.g.neotest_rust_init = true
-end
+    vim.g.neotest_rust_init = true
+  end
+end)
